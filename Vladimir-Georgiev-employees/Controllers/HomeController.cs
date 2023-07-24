a672546a8e90c07551d938772b5fb8c582096dfd
Vladimir Georgiev <vladimir.georgiev.dev@gmail.com>
2023-07-24 14:16:10 +0300

    Clean-up

--- Vladimir-Georgiev-employees/Controllers/HomeController.cs
+++ Vladimir-Georgiev-employees/Controllers/HomeController.cs
@@ -1,32 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
-using Vladimir_Georgiev_employees.Models;
 
 namespace Vladimir_Georgiev_employees.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
         }
 
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
--- Vladimir-Georgiev-employees/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Vladimir_Georgiev_employees.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}

751712ef8553558c3fc8b93ee3614bcae442a77e
Vladimir Georgiev <vladimir.georgiev.dev@gmail.com>
2023-07-24 14:14:23 +0300

    Add project files.

---
+++ Vladimir-Georgiev-employees/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Vladimir_Georgiev_employees.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
